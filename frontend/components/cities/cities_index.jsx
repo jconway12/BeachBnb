07b6c591a5490ddd8e37d0eb1285e52726856ca6
App Academy Student <aa@Apps-Mac-mini-8.local>
2019-10-04 13:50:22 -0400

    add scroll to city index

--- frontend/components/cities/cities_index.jsx
+++ frontend/components/cities/cities_index.jsx
@@ -5,21 +5,36 @@ import {fetchListings, getCitiesFromListings } from '../../actions/listing_actio
 class CitiesIndex extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { cities: [] }
+    this.state = { cities: [], lowerIdx: 0, uppderIdx: 5 }
+    this.incrementRange = this.incrementRange.bind(this);
+    this.decrementRange = this.decrementRange.bind(this);
   }
+
   componentDidMount() {
     this.props.fetchListings();
+  }
+
+  incrementRange() {
+     this.setState({lowerIdx: this.state.lowerIdx + 1, uppderIdx: this.state.uppderIdx + 1});
+  }
+
+  decrementRange() {
+    if (this.state.lowerIdx > 0) {
+      this.setState({ lowerIdx: this.state.lowerIdx - 1, uppderIdx: this.state.uppderIdx - 1 });
+    }
   }
 
   render() {
     let cities;
     if (this.props.listings) {
-      cities = this.props.getCities(this.props.listings)
+      cities = this.props.getCities(this.props.listings).slice(this.state.lowerIdx, this.state.uppderIdx);
     } else {
        cities = [];
     }
     return (
+      <>
       <div className="cities-container">
+          <div id="decrementer" onClick={this.decrementRange}>{"<"}</div>
         {cities.map((city, idx) => {
           return (
             <div key={idx} className="city-element">
@@ -29,7 +44,9 @@ class CitiesIndex extends React.Component {
             </div>
           )
         })}
+        <div id="incrementer" onClick={this.incrementRange}>{">"}</div>
       </div>
+      </>
     )
   }
 }
