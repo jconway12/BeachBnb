24afcac6f03e0e9010fe19933f1c6a0ead418b13
Jesse Conway <jessicaeconway12@gmail.com>
2019-10-10 16:38:14 -0400

    add host reservation show outline

---
+++ frontend/components/reservations/host_reservations_show.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class HostReservationShow extends React.Component {
+    //will need listing passed down as props (should have reservation Ids)
+    //will need fetchReservations method
+    render() {
+        return null;
+    }
+}
+
+export default HostReservationShow;
