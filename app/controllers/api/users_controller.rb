69131cd8333659b3b3277e28fff457a9b5a132db
Jesse Conway <jessicaeconway12@gmail.com>
2019-10-10 16:28:08 -0400

    check session errors

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,11 +5,13 @@ class Api::UsersController < ApplicationController
   end
 
   def create
+    # debugger
     @user = User.new(user_params)
     if @user.save
       login!(@user)
       render :show
     else
+      # debugger
       render json: @user.errors.full_messages, status: 422
     end
   end
--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,10 +1,15 @@
 import {RECEIVE_SESSION_ERRORS, RESET_SESSION_ERRORS} from '../actions/session_actions';
 
 const SessionErrorsReducer = (state = [], action) => {
+  // debugger
   Object.freeze(state);
   switch(action.type) {
     case RECEIVE_SESSION_ERRORS: {
+      if (action.errors.responseJSON) {
       return action.errors.responseJSON;
+      } else {
+        return ["Invalid Crednetials"]; //FIX
+      }
     }
     case RESET_SESSION_ERRORS: {
       return [];
