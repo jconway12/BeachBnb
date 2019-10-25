0b55fc525a77cc050f7394cedd1fca679eade31f
Jesse Conway <jessicaeconway12@gmail.com>
2019-10-25 15:37:50 -0400

    add angel list link

--- frontend/components/home.jsx
+++ frontend/components/home.jsx
@@ -20,6 +20,7 @@ const Home = props => {
         <footer id='home-footer'>
         <a href="https://github.com/jconway12/BeachBnb"><div><img src={window.gitURL} /></div></a>
           <a href="https://www.linkedin.com/in/jessica-conway-35120815b"><div><img src={window.linkedinURL}/></div></a>
+          <a href="https://angel.co/jessica-conway-1"><div><img src={window.angellistURL} /></div></a>
         </footer>
       </div>
     </>
